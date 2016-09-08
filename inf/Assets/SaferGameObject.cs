6ed7dae1ce2183e9e4afc516a5253084fdd8050b
Erik Svedäng <erik.svedang@gmail.com>
2016-09-08 10:31:34 +0200

    safer game object

---
+++ inf/Assets/SaferGameObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public static class SaferGameObject
+{
+	public static T FindObjectOfType<T>() where T : class {
+		T o = GameObject.FindObjectOfType(typeof(T)) as T;
+		if(o == null) {
+			throw new UnityException(string.Format("No instance of {0} could be found in the scene", typeof(T).ToString()));
+		}
+		return o;
+	}
+}
